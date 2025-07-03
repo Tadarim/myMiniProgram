a9d69601849cdb436b2497554c537a341f757e8c
Tadarim <2560285440@qq.com>
2025-07-03 10:34:01 +0800

    feat: 配置项删除

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "xuxinyu1234",
+  password: "password",
   database: "learning_system",
   waitForConnections: true,
   connectionLimit: 10,
--- server/src/utils/token.ts
+++ server/src/utils/token.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "xuxinyu.tadarim";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export interface TokenPayload {
   id: number;
